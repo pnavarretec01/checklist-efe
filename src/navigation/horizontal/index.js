d5fda4947da5b337db273fdecbf05da7ec4a9ace
pnavarretec01 <patricionavarrete.c01@gmail.com>
2023-08-14 12:40:44 -0400

    first commit

---
+++ src/navigation/horizontal/index.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: 'Home',
+    to: { name: 'index' },
+    icon: { icon: 'tabler-smart-home' },
+  },
+  {
+    title: 'Segunda página',
+    to: { name: 'second-page' },
+    icon: { icon: 'tabler-file' },
+  },
+]
---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <VCard class="mb-6" title="Kick start your project 🚀">
+      <VCardText>
+        Enim eu laborum officia incididunt ea. Cupidatat do ea ullamco velit ut mollit. Qui fugiat sunt ullamco
+        aliqua ea ipsum enim voluptate.
+      </VCardText>
+      <VCardText>
+        Elit aute eiusmod sint esse est laborum velit Lorem commodo fugiat. Occaecat ipsum consequat dolor culpa nisi.
+        Consectetur nostrud ut cillum exercitation in occaecat nisi eiusmod ipsum. Consectetur dolor officia Lorem non ut.
+        Incididunt non fugiat consequat ea exercitation est incididunt qui laboris amet. Lorem anim fugiat quis
+        exercitation qui eu id dolor elit do veniam aute eu mollit.
+      </VCardText>
+    </VCard>
+  </div>
+</template>
